afd1ff8a4e5d90838cc6b5da3fd6812a0623af7b
Selvaraj <gd18@INMACJ2EMG.local>
2017-07-19 11:43:53 +0100

    test update

--- croc-hunter.go
+++ croc-hunter.go
@@ -47,6 +47,7 @@ const (
 				<strong>Release: </strong>%s<br>
 				<strong>Commit: </strong>%s<br>
 				<strong>Powered By: </strong>%s<br>
+				<strong>Ganesan S</strong>
 				</div>
 			</body>
 		</html>
